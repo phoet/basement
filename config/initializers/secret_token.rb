d9df8d9715bd804cd120b12e110585a19a3f8a45
phoet <phoetmail@googlemail.com>
2018-04-10 12:00:22 +0200

    fix warnings

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Basement::Application.config.secret_token = 'f8a9471da880325b4c880e031776cf827ab3fab17c9b057acb5b2dcf8f9030a2f96da9c7a2787ae43b8db038c1cb430e755c98a0732207a479e66b278226e09d'
+Basement::Application.config.secret_key_base = 'f8a9471da880325b4c880e031776cf827ab3fab17c9b057acb5b2dcf8f9030a2f96da9c7a2787ae43b8db038c1cb430e755c98a0732207a479e66b278226e09d'
